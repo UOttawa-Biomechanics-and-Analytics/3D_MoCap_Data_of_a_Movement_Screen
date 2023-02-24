707455019111387b7140a573ded3953ff31bb67f
Xiong <837321417@qq.com>
2023-02-24 12:41:45 -0500

    cleaned it up.

--- Rename_c3d.py
+++ Rename_c3d.py
@@ -3,8 +3,8 @@ import re
 
 
 def rename_files(root_path):
-    target_fld = "Generated_C3D_files"
-    participant_fld_list = os.listdir(root_path)
+    target_fld = "Generated_C3D_files"            # go the folder with .c3d files
+    participant_fld_list = os.listdir(root_path)  # dir all participants
     count = 0
 
     for parID in enumerate(participant_fld_list):
@@ -13,20 +13,16 @@ def rename_files(root_path):
         for root,dirs,files in os.walk(final_path, topdown=True):
             for file in files:
                 if file != "Calib1.c3d":
-                    new_file = re.split(r'(^[^\d]+)', file)[2:]
-                    # print(new_file)
+                    new_file = re.split(r'(^[^\d]+)', file)[2:]  # get rid of the names
                     new_file = new_file[0].split("-", 1)[0]
-                    # print(new_file)
 
                     old_name = os.path.join(final_path, file)
                     if new_file.endswith(".c3d"):
                         new_name = os.path.join(final_path, new_file)
-                        # print(new_name)
                         os.rename(old_name, new_name)
                     else:
-                        new_name = os.path.join(final_path, new_file +".c3d")
-                    # print(new_name)
-                        os.rename(old_name, new_name)
+                        new_name = os.path.join(final_path, new_file +".c3d") # add file extension
+                        os.rename(old_name, new_name)                         # rename the files
                 count += 1  
 
         print ('--------------------------------')
